4592e471cd8aacd70567b80acf080cb56a3fe7f2
muhammadabdullah-dev1 <muhammadabdullah53897@gmail.com>
2026-08-19 22:45:58 +0500

    upd added

--- app.js
+++ app.js
@@ -6,20 +6,36 @@ let todoList = document.querySelector("#todoList")
 addBtn.addEventListener("click", function (dets) {
     // dets.preventDefault()
 
-    if (todoInput.value !== "") {
+    if (todoInput.value === "") {
+        alert("first enter text")
+
+    } else {
         const li = document.createElement("li")
-        const span =document.createElement("span")
-        const delbtn =document.createElement("button")
-        const addbtn =document.createElement("button")
+        const span = document.createElement("span")
+        const delbtn = document.createElement("button")
+        delbtn.innerHTML = "Delete"
+
+
+        const updbtn = document.createElement("button")
+        updbtn.innerHTML = "Update"
+
         li.textContent = todoInput.value
         todoList.appendChild(li)
-        li.appendChild(addbtn)
-        li.appendChild(addbtn)
-         todoInput.value = ""
+        li.appendChild(span)
+        li.appendChild(delbtn)
+        li.appendChild(updbtn)
+        todoInput.value = ""
 
-    } 
+
+
+        updbtn.addEventListener("click", function () {
+            let newText = prompt("Update tex")
+            li.textContent = newText.value
+        })
+    }
 
 
     // appendchild(todoList.textContent = todoInput.value)
 
 })
+
